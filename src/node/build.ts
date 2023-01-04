f2fb06dfea544f4999a885447785f1357057667b
BILIBILI\v_zhangjiaming <https://git.bilibili.co/>
2023-01-04 10:38:46 +0800

    fix: commit message staged

--- src/node/build.ts
+++ src/node/build.ts
@@ -82,7 +82,7 @@ export async function build(root: string = process.cwd()) {
   // 引入 server-entry 模块
   const serverEntryPath = path.join(root, '.temp', 'ssr-entry.js');
   // 服务端渲染，产出 HTML str
-  console.log('test staged');
+  console.log('test lint-staged ');
 
   // const { render } = await import(serverEntryPath);
   const { render } = await import(pathToFileURL(serverEntryPath));
